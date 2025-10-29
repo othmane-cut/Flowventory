5dd3750313583fd594cbfdfad464d6dffeb013c7
othmane <lheothmane@gmail.com>
2025-10-29 11:05:46 +0100

    creat the controller

---
+++ src/main/java/com/flowventorygrp/flowventory/pos/controller/ProductController.java
@@ -0,0 +1,70 @@
+package com.flowventorygrp.flowventory.pos.controller;
+
+
+
+import com.flowventorygrp.flowventory.pos.model.Product;
+import com.flowventorygrp.flowventory.pos.model.ProductType;
+import com.flowventorygrp.flowventory.pos.repository.ProductRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("products", productRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/product/new")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        model.addAttribute("types", ProductType.values());
+        return "newProduct";
+    }
+
+    @PostMapping("/product")
+    public String saveProduct(@Valid @ModelAttribute("product") Product product, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("types", ProductType.values());
+            return "newProduct";
+        }
+        productRepository.save(product);
+        return "redirect:/";
+    }
+
+    @GetMapping("/product/edit/{id}")
+    public String editProduct(@PathVariable Long id, Model model) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Produit invalide : " + id));
+        model.addAttribute("product", product);
+        model.addAttribute("types", ProductType.values());
+        return "editProduct";
+    }
+
+    @PostMapping("/product/{id}")
+    public String updateProduct(@PathVariable Long id,
+                                @Valid @ModelAttribute("product") Product product,
+                                BindingResult bindingResult,
+                                Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("types", ProductType.values());
+            return "editProduct";
+        }
+        productRepository.save(product);
+        return "redirect:/";
+    }
+    @GetMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id, Model model) {
+        productRepository.deleteById(id);
+        return "redirect:/";}
+
+}
